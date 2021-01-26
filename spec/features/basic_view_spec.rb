b59dbe3bd9dc5fefa15f598064624237e4920da7
mkfuget <michaelfuget@gmail.com>
2021-01-25 16:32:53 -0800

    Adding test for adding nonexisting cards

--- spec/features/basic_view_spec.rb
+++ spec/features/basic_view_spec.rb
@@ -29,7 +29,7 @@ describe 'App' do
     describe "logging in" do 
         it "displays the users collection page if username and password is given" do
             @user = User.create(:username => "Jack", :password => "Olantern")
-            visit '/login'
+            visit '/users/login'
             fill_in "username", :with => "Jack"
             fill_in "password", :with => "Olantern"
             click_button "Log In"
@@ -37,8 +37,48 @@ describe 'App' do
             expect(page.status_code).to eq(200)
             expect(page.body).to include("Collection")
         end
-      
-      
+    end
+    describe "User Actions" do 
+      before(:each) do
+        CardInstance.destroy_all
+        Deck.destroy_all
+        User.destroy_all  
+        @user = User.create(:username => "Jack", :password => "Olantern")
+        visit '/users/login'
+        fill_in "username", :with => "Jack"
+        fill_in "password", :with => "Olantern"
+        click_button "Log In"
+      end
+  
+      describe "adding card to collection" do 
+          it "shows a card with the correct count after adding it to the collection" do 
+            visit '/card_instances/new'
+            fill_in "name", :with => "Loathsome Chimera"
+            fill_in "count", :with => "4"
+            click_button "Create Card Instance"
+            expect(page.current_path).to eq('/card_instances/new')
+            visit '/card_instances'
+
+            expect(page.body).to include("Loathsome Chimera")
+            expect(page.body).to include("4")
+
+          end
+
+          it "Doesnt add a card that is not in the collection" do 
+            visit '/card_instances/new'
+            fill_in "name", :with => "Hero of Precinct One"
+            fill_in "count", :with => "4"
+            click_button "Create Card Instance"
+            expect(page.current_path).to eq('/card_instances/new')
+            expect(page.body).to include("Addition failed card not found")
+
+            visit '/card_instances'
+
+            expect(page.body).not_to include("Hero of Precinct One")
+
+          end
 
     end
+  end
+
 end  
